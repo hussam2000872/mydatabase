d043308c56333662fbaefac0357a2e0e9c42f0d4
HÜSAM MUFLIH <64381157+hussam2000872@users.noreply.github.com>
2024-05-14 11:38:28 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\hussa\\OneDrive\\Desktop\\New folder (2)\\my_atabase" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\hussa\\OneDrive\\Desktop\\New folder (2)\\my_atabase"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\hussa\\OneDrive\\Desktop\\New folder (2)\\my_atabase\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\hussa\\OneDrive\\Desktop\\New folder (2)\\my_atabase"
+  "FLUTTER_TARGET=C:\\Users\\hussa\\OneDrive\\Desktop\\New folder (2)\\my_atabase\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNDgxN2M5OWM5ZmQ0OTU2ZjI3NTA1MjA0ZjdlMzQ0MzM1ODEwYWVkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\hussa\\OneDrive\\Desktop\\New folder (2)\\my_atabase\\.dart_tool\\package_config.json"
+)
